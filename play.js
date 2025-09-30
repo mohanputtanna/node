683e0a1cbbd5299ea97b0f1e9f77dc2ea52ef2a5
Mohan Puttanna <mohan.puttanna@jll.com>
2025-09-30 09:14:06 +0530

    initial max commit

---
+++ play.js
@@ -0,0 +1,9 @@
+var name = 'Mohan'
+var age = 34
+var hashHobbies = true;
+
+function summarizeUser(userName, userAge, userHasHobbies) {
+    return ('Name is ' + userName + ' Age is ' + userAge + ' and the user has hobbies: ' + userHasHobbies);
+}
+
+console.log(summarizeUser(name, age, hashHobbies));
