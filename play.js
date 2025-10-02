93e348b2f4cb0260c8b89938af691dfed153557a
Mohan Puttanna <mohan.puttanna@jll.com>
2025-10-02 10:48:25 +0530

    add object

--- play.js
+++ play.js
@@ -6,4 +6,19 @@ function summarizeUser(userName, userAge, userHasHobbies) {
     return ('Name is ' + userName + ' Age is ' + userAge + ' and the user has hobbies: ' + userHasHobbies);
 }
 
-console.log(summarizeUser(name, age, hashHobbies));
+
+const person = {
+    name : 'Mohan',
+    age : 34,
+    hobbies : ['Sports', 'Cooking'],
+    greet() {
+        console.log('Hi, I am ' + this.name);
+    }
+}
+
+const summarizeUserFunc = (userName, userAge, userHasHobbies) => ('Name is ' + userName + ' Age is ' + userAge + ' and the user has hobbies: ' + userHasHobbies);
+
+// console.log(summarizeUser(name, age, hashHobbies));
+// console.log(summarizeUserFunc(name, age, hashHobbies));
+
+person.greet()
