1f506e38a98f832016046276958b2f6a7e3e6358
Mohan Puttanna <mohan.puttanna@jll.com>
2025-10-21 16:30:16 +0530

    add styling to the pages

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404-error.html'));
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 
